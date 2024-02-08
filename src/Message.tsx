5d2e38694ca89b97c019fa06fdaa410c2352c745
Samantha <samanthazakir@gmail.com>
2024-02-08 20:30:47 +0800

    adding a message in react

---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+// PascalCasing
+function Message() {
+    return <h1>Hello WOrldfrom Message compononet</h1>;
+}
+export default Message;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Message from './Message'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Message/>
   </React.StrictMode>,
 )
